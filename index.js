b4a972f5e164b201ef3cea093dc0c1b71fc5d2b6
xangmarques <xang.marques@gmail.com>
2023-02-11 00:16:36 -0300

    Conexão direta SQL Server Node.js

---
+++ index.js
@@ -0,0 +1,24 @@
+const sql = require("mssql/msnodesqlv8")
+
+var config= {
+    server:"DESKTOP-19A3TOA\\MIS",
+    database:"RH",
+    driver:"msnodesqlv8",
+    user:"sa",
+    password:"Mis@0000",
+        options:{
+            trustedConnection:true,
+        }
+}
+
+sql.connect(config,function(err){
+    if(err)console.log(err);
+    var request=new sql.Request();
+request.query("Select  * from tblfuncionarios WHERE MATRICULA=1048",function(err,records){
+    if(err)console.log(err);
+    else
+    console.log(records);
+
+})
+
+})
